ce0ad0baf99a16274fd99e7614ac560c56b8d972
PolarWolf314 <aaronjoshguo@gmail.com>
2025-01-16 16:28:53 +1300

    feat: install button and card + hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,25 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
 export default function Home() {
-  return <div>Hello World</div>;
+  return (
+    <>
+      <h1>Taekwon-Do is an ART</h1>
+      <Card>
+        <CardContent>
+          <p>Do you want to train your mind and body?</p>
+        </CardContent>
+        <CardFooter>
+          <Button>Join Us</Button>
+        </CardFooter>
+      </Card>
+    </>
+  );
 }

165610748abfc4d3ecc0c0296a844b2ede8e0c7a
PolarWolf314 <aaronjoshguo@gmail.com>
2025-01-28 16:06:45 +1300

    feat: add a menu sheet for mobile view

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 
 import Logo from "@/components/layout/header/Logo";
+import MenuSheet from "./header/MenuSheet";
 
 const Header = () => {
   return (
     <header className="bg-gray-200">
       <nav>
         <Logo />
+        <MenuSheet />
       </nav>
     </header>
   );

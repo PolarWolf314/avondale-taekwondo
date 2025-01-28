626f1f892e172215e34f427c35a2fb1ef325a78f
PolarWolf314 <aaronjoshguo@gmail.com>
2025-01-28 17:24:35 +1300

    feat: finish styling header

--- src/components/layout/header/ExpandedLinks.tsx
+++ src/components/layout/header/ExpandedLinks.tsx
@@ -1,6 +1,30 @@
+import { Button } from "@/components/ui/button";
 import React from "react";
 
 const ExpandedLinks = () => {
-  return <div>ExpandedLinks</div>;
+  return (
+    <ul className="flex flex-row gap-0.5 md:gap-3">
+      <li>
+        <Button variant="link">
+          <a href="/">Home</a>
+        </Button>
+      </li>
+      <li>
+        <Button variant="link">
+          <a href="/about">About</a>
+        </Button>
+      </li>
+      <li>
+        <Button variant="link">
+          <a href="/pricing">Pricing and FAQ</a>
+        </Button>
+      </li>
+      <li>
+        <Button>
+          <a href="/join">Join</a>
+        </Button>
+      </li>
+    </ul>
+  );
 };
 export default ExpandedLinks;

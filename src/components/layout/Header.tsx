633e3461cc4ef5bc944c7feec936406f448bce44
PolarWolf314 <aaronjoshguo@gmail.com>
2025-01-28 15:29:48 +1300

    feat: style logo and update images

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -2,11 +2,20 @@ import React from "react";
 
 const Header = () => {
   return (
-    <>
-      <div className="font-display font-bold tracking-tight">
-        Avondale Taekwon-Do
-      </div>
-    </>
+    <header className="bg-gray-200">
+      <nav>
+        <a className="flex flex-row gap-2 p-4 items-center w-fit" href="/">
+          <span className="font-display font-bold tracking-tight text-xl">
+            Avondale Taekwon-Do
+          </span>
+          <img
+            src="/assets/avondale_taekwondo_logo.png"
+            alt="The Avondale Taekwon-Do logo. It is a black circle with white text and imagery. The word Avondale is at the top, and Taekwon-Do on the bottom. In the center of the circle is a silhouette of a man in uniform performing a turning kick"
+            className="h-8"
+          />
+        </a>
+      </nav>
+    </header>
   );
 };
 

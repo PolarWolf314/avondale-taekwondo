50a65d0e695a93f49ed398112c08ebdcd70c413b
PolarWolf314 <aaronjoshguo@gmail.com>
2025-01-29 20:23:00 +1300

    feat: add footer links component and populate them

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,24 +1,47 @@
 import { Facebook } from "lucide-react";
 import React from "react";
+import FooterLinks from "./footer/FooterLinks";
 
 const Footer = () => {
   return (
     <footer>
       THIS IS THE FOOTER
       <div>Logo, contact details, and address</div>
-      <div>
-        Links for the page. That is, home, about us, pricing and FAQ, and
-        contact us
-      </div>
-      <div>
-        Links to itkd stuff. That is, the main website, members page, and events
-        page, and any other resources such as a digital copy of the handbook
-      </div>
-      <div>
-        Legal and policy. Privacy policy (we do not track anything), link to
-        itkd terms and conditions, and write up a code of conduct page with the
-        instructors code of conduct + tenents of tkd
-      </div>
+      <FooterLinks
+        title="Club"
+        links={[
+          { name: "Home", url: "/" },
+          { name: "About Us", url: "/about" },
+          { name: "Pricing and FAQ", url: "/pricing" },
+          { name: "Contact Us", url: "/join" },
+        ]}
+      />
+      <FooterLinks
+        title="International TKD"
+        links={[
+          { name: "Members", url: "https://members.itkd.co.nz/" },
+          { name: "News", url: "https://members.itkd.co.nz/news/" },
+          { name: "Events", url: "https://members.itkd.co.nz/events/" },
+          {
+            name: "Handbook",
+            url: "https://tigertkd.co.nz/wp-content/uploads/2020/06/Technqiues-Handbook-2017.pdf",
+          },
+        ]}
+      />
+      <FooterLinks
+        title="Legal and Policy"
+        links={[
+          { name: "Privacy Policy", url: "/privacy-policy" },
+          {
+            name: "Terms and Conditions",
+            url: "https://members.itkd.co.nz/terms.php",
+          },
+          {
+            name: "Code of Conduct",
+            url: "code-of-conduct",
+          },
+        ]}
+      />
       <hr />
       <div>
         &copy; Avondale Taekwon-Do {new Date().getFullYear()}. All rights

5c589d6503908ea62a49fdb43eaa36b3db579917
PolarWolf314 <aaronjoshguo@gmail.com>
2025-01-29 19:54:32 +1300

    feat: add outline of what to write

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,10 +1,32 @@
+import { Facebook } from "lucide-react";
 import React from "react";
 
 const Footer = () => {
   return (
-    <>
-      <div>This is the footer</div>
-    </>
+    <footer>
+      THIS IS THE FOOTER
+      <div>Logo, contact details, and address</div>
+      <div>
+        Links for the page. That is, home, about us, pricing and FAQ, and
+        contact us
+      </div>
+      <div>
+        Links to itkd stuff. That is, the main website, members page, and events
+        page, and any other resources such as a digital copy of the handbook
+      </div>
+      <div>
+        Legal and policy. Privacy policy (we do not track anything), link to
+        itkd terms and conditions, and write up a code of conduct page with the
+        instructors code of conduct + tenents of tkd
+      </div>
+      <hr />
+      <div>
+        &copy; Avondale Taekwon-Do {new Date().getFullYear()}. All rights
+        reserved. Designed and created by Aaron Guo. Check out my GitHub for
+        more of my work!
+      </div>
+      <Facebook />
+    </footer>
   );
 };
 

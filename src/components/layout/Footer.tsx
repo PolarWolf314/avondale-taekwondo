1726e66b85c9720710b2c2fe4a282ba13b723cbf
Aaron Guo <89121695+PolarWolf314@users.noreply.github.com>
2025-01-29 21:26:42 +1300

    feat: footer (#21)
    * feat: create footer and add to layout
    
    * feat: add outline of what to write
    
    * feat: add footer links component and populate them
    
    * feat: add basic club information
    
    * feat: style the footer

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,92 @@
+import { Facebook } from "lucide-react";
+import React from "react";
+import FooterLinks from "./footer/FooterLinks";
+import Logo from "./header/Logo";
+import Link from "next/link";
+
+const Footer = () => {
+  return (
+    <footer className="px-6 lg:px-24 py-8 bg-gray-200">
+      <section className="flex flex-col md:flex-row md:justify-between md:pb-4">
+        <section className="flex flex-col gap-4">
+          <Logo />
+          <p className="text-sm">instructor@avondaletkd.co.nz</p>
+          <p className="text-sm">+64 21 164 6158</p>
+          <Link
+            className="text-sm"
+            href="https://maps.app.goo.gl/yD6BswpVuXgaqF2w7"
+          >
+            Avondale Primary School
+            <br />
+            Crayford Street West, Avondale
+            <br />
+            Auckland 1026, NZ
+          </Link>
+          <Link
+            className="flex flex-row items-center gap-4"
+            href="https://www.facebook.com/AvondaleTKD"
+          >
+            <div className="text-sm font-semibold">Follow us</div>
+            <span>
+              <Facebook className="h-8 w-8" />
+            </span>
+          </Link>
+        </section>
+
+        <section className="flex flex-col sm:flex-row gap-4 sm:gap-16 md:gap-10 lg:gap-20 xl:gap-24 py-8">
+          <FooterLinks
+            title="Club"
+            links={[
+              { name: "Home", url: "/" },
+              { name: "About Us", url: "/about" },
+              { name: "Pricing and FAQ", url: "/pricing" },
+              { name: "Contact Us", url: "/join" },
+            ]}
+          />
+          <FooterLinks
+            title="International TKD"
+            links={[
+              { name: "Members", url: "https://members.itkd.co.nz/" },
+              { name: "News", url: "https://members.itkd.co.nz/news/" },
+              { name: "Events", url: "https://members.itkd.co.nz/events/" },
+              {
+                name: "Handbook",
+                url: "https://tigertkd.co.nz/wp-content/uploads/2020/06/Technqiues-Handbook-2017.pdf",
+              },
+            ]}
+          />
+          <FooterLinks
+            title="Legal and Policy"
+            links={[
+              { name: "Privacy Policy", url: "/privacy-policy" },
+              {
+                name: "Terms and Conditions",
+                url: "https://members.itkd.co.nz/terms.php",
+              },
+              {
+                name: "Code of Conduct",
+                url: "code-of-conduct",
+              },
+            ]}
+          />
+        </section>
+      </section>
+
+      <section className="flex flex-col md:flex-row md:justify-between gap-2 text-gray-700 border-t-2 border-gray-400 pt-6">
+        <p className="text-sm">
+          &copy; Avondale Taekwon-Do {new Date().getFullYear()}. All rights
+          reserved.
+        </p>
+        <p className="text-sm">
+          Designed and created by Aaron Guo ❤️ Check out my{" "}
+          <span className="underline">
+            <Link href="https://github.com/PolarWolf314">GitHub</Link>
+          </span>{" "}
+          for more of my work!
+        </p>
+      </section>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/layout/footer/FooterLinks.tsx
@@ -0,0 +1,41 @@
+/**
+ * A section of links in the footer.
+ *
+ * @param title (string): The title of the section
+ * @param links (array): An array of objects with a name and url property
+ *
+ * This component is a section of links in the footer. It takes a title and an array of links
+ * and renders them in a column. This component is used in the footer to display links to
+ * different sections of the website.
+ */
+
+import Link from "next/link";
+
+interface Link {
+  name: string;
+  url: string;
+}
+
+interface FooterLinksProps {
+  title: string;
+  links: Link[];
+}
+
+const FooterLinks: React.FC<FooterLinksProps> = ({ title, links }) => {
+  /**
+   * Maps over the links array and renders each link as a nextjs Link.
+   */
+  const renderedLinks = links.map((link, index) => (
+    <Link key={index} href={link.url} className="text-sm">
+      {link.name}
+    </Link>
+  ));
+  return (
+    <div className="flex flex-col gap-2">
+      <h3 className="text-lg font-bold">{title}</h3>
+      <>{renderedLinks}</>
+    </div>
+  );
+};
+
+export default FooterLinks;

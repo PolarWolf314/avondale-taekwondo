4e56a5ce5c103dbed448b87231f23227ece23858
PolarWolf314 <aaronjoshguo@gmail.com>
2025-01-31 20:07:16 +1300

    feat: change text on page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,51 +12,57 @@ export default function Home() {
         <Card className="md:w-96 lg:w-[38rem] place-self-start px-3 py-2 rounded-none">
           <CardHeader>
             <CardTitle className="text-lg">
-              Discover the art of Taekwon-Do
+              Come for the Taekwon-Do, stay for the{" "}
+              <span className="italic">community</span>
             </CardTitle>
           </CardHeader>
           <CardContent>
             <p>
-              Explore the fundamental techniques of Taekwon-Do, such as kicks,
-              punches, and blocks. Learn the proper forms and stances to execute
-              powerful and precise movements. Whether you&apos;re a beginner or
-              an experienced practitioner, our expert instructors will guide you
-              on the path to mastery.
+              At Avondale Taekwon-Do, we believe that anybody can become the
+              best version of themselves through hard work and determination. We
+              are one of the few remaining schools of Taekwon-Do in Aotearoa to
+              still stand by having everyone in the family train together.
+              <br />
+              <br />
+              When you put on the uniform, you're not just a student of
+              Taekwon-Do. You're part of a{" "}
+              <span className="italic">community</span>.
             </p>
           </CardContent>
         </Card>
         <Card className="md:w-96 lg:w-[38rem] place-self-end px-3 py-2 rounded-none">
           <CardHeader>
             <CardTitle className="text-lg">
-              Unlock your inner strength
+              Self defense in the modern age
             </CardTitle>
           </CardHeader>
           <CardContent>
             <p>
-              Taekwon-Do offers a multitude of benefits beyond physical fitness.
-              Experience improved focus, discipline, and self-confidence as you
-              progress through your training. Enhance your mental and emotional
-              well-being while developing a strong and resilient spirit.
-              Discover how Taekwon-Do can positively impact your life on and off
-              the training mat.
+              In today’s world, knowing how to defend yourself is more important
+              than ever. At Avondale Taekwon-Do, we teach practical self-defense
+              techniques that equip you with the confidence to handle real-life
+              situations. Beyond physical skills, we emphasise awareness,
+              de-escalation, and mental resilience—helping you stay safe in any
+              environment.
             </p>
           </CardContent>
         </Card>
         <Card className="md:w-96 lg:w-[38rem] place-self-start px-3 py-2 rounded-none">
           <CardHeader>
             <CardTitle className="text-lg">
-              Tradition, discipline, respect
+              Courtesy, integrity, perseverance, self-control, indomitable
+              spirit
             </CardTitle>
           </CardHeader>
           <CardContent>
             <p>
-              Immerse yourself in the essence of Taekwon-Do—a martial art deeply
-              rooted in tradition, discipline, and respect. Experience the
-              ancient wisdom passed down through generations as you train with
-              our passionate instructors. Discover the profound values of
-              courtesy, integrity, perseverance, self control, and indomitable
-              spirit. Foster a personal growth and a sense of honour within our
-              Taekwon-Do community.
+              These five tenets of Taekwon-Do shape not just how we train, but
+              how we live. Courtesy teaches us respect for others, integrity
+              keeps us honest in our actions, perseverance drives us to push
+              through challenges, self-control strengthens our discipline, and
+              indomitable spirit empowers us to stand firm in the face of
+              adversity. At Avondale Taekwon-Do, we don't just practice martial
+              arts — we build character.
             </p>
           </CardContent>
         </Card>

80a42a2e7bb1a5f5b71ac3bfc19641d4308987e6
Aaron Guo <89121695+PolarWolf314@users.noreply.github.com>
2025-01-31 20:29:53 +1300

    feat: landing (#25)
    * perf: compress all images
    
    * feat: add background color to every page
    
    * feat: style cards on main page
    
    * feat: style buttons
    
    * feat: change text on page
    
    * refactor: add space
    
    * fix: escape apostrophes
    
    * fix: missed an apostrophe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,62 +1,81 @@
 import { Hero } from "@/components/layout";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
       <Hero />
 
-      <Card className="mt-32">
-        <CardHeader>
-          <CardTitle>Discover the art of Taekwon-Do</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <p>
-            Explore the fundamental techniques of Taekwon-Do, such as kicks,
-            punches, and blocks. Learn the proper forms and stances to execute
-            powerful and precise movements. Whether you&apos;re a beginner or an
-            experienced practitioner, our expert instructors will guide you on
-            the path to mastery.
-          </p>
-        </CardContent>
-      </Card>
-      <Card>
-        <CardHeader>
-          <CardTitle>Unlock your inner strength</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <p>
-            Taekwon-Do offers a multitude of benefits beyond physical fitness.
-            Experience improved focus, discipline, and self-confidence as you
-            progress through your training. Enhance your mental and emotional
-            well-being while developing a strong and resilient spirit. Discover
-            how Taekwon-Do can positively impact your life on and off the
-            training mat.
-          </p>
-        </CardContent>
-      </Card>
-      <Card>
-        <CardHeader>
-          <CardTitle>Tradition, discipline, respect</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <p>
-            Immerse yourself in the essence of Taekwon-Do—a martial art deeply
-            rooted in tradition, discipline, and respect. Experience the ancient
-            wisdom passed down through generations as you train with our
-            passionate instructors. Discover the profound values of courtesy,
-            integrity, perseverance, self control, and indomitable spirit.
-            Foster a personal growth and a sense of honour within our Taekwon-Do
-            community.
-          </p>
-        </CardContent>
-      </Card>
+      <section className="mt-32 grid flex-col w-screen gap-8 lg:gap-12 px-8 sm:px-16 lg:px-32">
+        <Card className="md:w-96 lg:w-[38rem] place-self-start px-3 py-2 rounded-none">
+          <CardHeader>
+            <CardTitle className="text-lg">
+              Come for the Taekwon-Do, stay for the{" "}
+              <span className="italic">community</span>
+            </CardTitle>
+          </CardHeader>
+          <CardContent>
+            <p>
+              At Avondale Taekwon-Do, we believe that anybody can become the
+              best version of themselves through hard work and determination. We
+              are one of the few remaining schools of Taekwon-Do in Aotearoa to
+              still stand by having everyone in the family train together.
+              <br />
+              <br />
+              When you put on the uniform, you&apos;re not just a student of
+              Taekwon-Do. You&apos;re part of a{" "}
+              <span className="italic">community</span>.
+            </p>
+          </CardContent>
+        </Card>
+        <Card className="md:w-96 lg:w-[38rem] place-self-end px-3 py-2 rounded-none">
+          <CardHeader>
+            <CardTitle className="text-lg">
+              Self defense in the modern age
+            </CardTitle>
+          </CardHeader>
+          <CardContent>
+            <p>
+              In today’s world, knowing how to defend yourself is more important
+              than ever. At Avondale Taekwon-Do, we teach practical self-defense
+              techniques that equip you with the confidence to handle real-life
+              situations. Beyond physical skills, we emphasise awareness,
+              de-escalation, and mental resilience — helping you stay safe in
+              any environment.
+            </p>
+          </CardContent>
+        </Card>
+        <Card className="md:w-96 lg:w-[38rem] place-self-start px-3 py-2 rounded-none">
+          <CardHeader>
+            <CardTitle className="text-lg">
+              Courtesy, integrity, perseverance, self-control, indomitable
+              spirit
+            </CardTitle>
+          </CardHeader>
+          <CardContent>
+            <p>
+              These five tenets of Taekwon-Do shape not just how we train, but
+              how we live. Courtesy teaches us respect for others, integrity
+              keeps us honest in our actions, perseverance drives us to push
+              through challenges, self-control strengthens our discipline, and
+              indomitable spirit empowers us to stand firm in the face of
+              adversity. At Avondale Taekwon-Do, we don&apos;t just practice
+              martial arts — we build character.
+            </p>
+          </CardContent>
+        </Card>
+      </section>
 
-      <div>
-        <Button>Learn more about us</Button>
-        <Button>Contact us</Button>
-      </div>
+      <section className="flex flex-row gap-4 sm:gap-16 md:gap-24 lg:gap-32 justify-center my-10 md:my-16 lg:my-24">
+        <Button className="text-md p-6 lg:p-12">
+          <Link href="/about">Learn more about us</Link>
+        </Button>
+        <Button className="text-md p-6 lg:p-12 hover:bg-purple-500">
+          <Link href="/join">Enquire now</Link>
+        </Button>
+      </section>
     </>
   );
 }

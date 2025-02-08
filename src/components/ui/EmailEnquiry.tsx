42996c6402760245fcd04406e13d66ff0d03af9b
PolarWolf314 <aaronjoshguo@gmail.com>
2025-02-08 15:36:52 +1300

    build: add select component from shadcn

--- src/components/ui/EmailEnquiry.tsx
+++ src/components/ui/EmailEnquiry.tsx
@@ -1,4 +1,11 @@
+"use client";
+
 import React from "react";
+import { z } from "zod";
+
+const emailFormSchema = z.object({
+  name: z.string().min(1, { message: "Please provide a name" }).max(50),
+});
 
 const EmailEnquiry = () => {
   return <div>EmailEnquiry</div>;

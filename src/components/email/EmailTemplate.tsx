a0d8d2277c5e6efe4fa010187a21d70d860a9702
Aaron Guo <89121695+PolarWolf314@users.noreply.github.com>
2025-07-31 21:22:53 +1200

    feat: create email template

---
+++ src/components/email/EmailTemplate.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const EmailTemplate = () => {
+  return (
+    <body>
+      <header>Email header</header>
+      <section>Email content</section>
+      <footer>Email footer</footer>
+    </body>
+  );
+};
+
+export default EmailTemplate;
